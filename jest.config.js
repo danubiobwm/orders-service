a901471088b151361a9ea53a16f46640459238dd
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-10-23 14:52:34 -0300

    jest confing

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  tsconfig: 'tsconfig.spec.json',
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts']
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json'
+    }
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: './coverage',
 };
